17d3083a0a39728b468729d0f78cb7fed3dd92da
dimitris <iracleous@gmail.com>
2024-06-21 16:30:32 +0300

    Add project files.

---
+++ mvcathtech/Models/EshopDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mvcathtech.Models
+{
+    public class EshopDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public EshopDbContext()
+        {
+        }
+
+        public EshopDbContext(DbContextOptions<EshopDbContext> options)
+       : base(options)
+        {
+        }
+
+    }
+}
